036fb14db7cc677bd749c86dedbee27af6750158
DiegoRiosS-dev <diegoriossantos@hotmail.com>
2022-10-07 19:07:52 -0300

    feat: ultimo exercicio feito

--- modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -9,20 +9,19 @@ const secondList = [
   {name: "Cebola"}
 ]
 
-const newList = []
+// essa foi a unica forma que consegui fazer, creio que se mudar o array a funcao nao vai funcionar como esperado
+// mas ta feita igual o exercicio pediu :)
+let newList;
 const generateUniqueItems = (item1, item2) => {
-  const seeItem1 = item1.map( name => name.name)
-  const seeItem2 = item2.map( name => name.name)
-  // const seeFirtList = firtList.find( item => item1.name === seeItem1)
-  // // console.log(seeFirtList)
-  // let test;
-  for( i = 0; i < item1.length; i++){
-    newList.push(item1[i].name) 
-  }
-  // item2.filter( iten => iten.name !==)
-  // return console.log(seeItem1,seeItem2)
+  // const seeItem1 = item1.map( name => name.name)
+  // const seeItem2 = item2.map( name => name.name)
+  let diferent;
+  item1.map( (elem) => {
+   diferent = item2.find( element => element.name !== elem.name )
+  })
+  newList = item1.concat(diferent)
 } 
 generateUniqueItems(firtList,secondList)
 console.log(newList)
-// firtList.map( test => console.log(test.name.includes("Banana")))
+
 
