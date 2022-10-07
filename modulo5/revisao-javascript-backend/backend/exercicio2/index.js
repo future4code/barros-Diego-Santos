33430b9cbd1208e7d76f14a1dd4850d4042c3700
DiegoRiosS-dev <diegoriossantos@hotmail.com>
2022-10-06 22:45:25 -0300

    feat: exercicio 2 e 3 feito

--- modulo5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,1 +1,39 @@
-console.log("exercício 2");
+//exercicio 1 Cadastro de cliente //
+const clients = [
+  {id: 1, name: "Fulano"},
+  {id: 2, name: "Ciclano"},
+  {id: 3, name: "Beltrano"},
+  {id: 4, name: "Fulana"}
+]
+const newClient = (id,name) => {
+  let client = clients.find( client => client.id === id) // faz a verificaçao se aquele id já existe e me retorna o objeto em sí //
+  const regex = /\W|_/; // faz a verificação se existe algum caractere //
+  if(client && client.id === id) {
+    return  `Erro. Parâmetro inválido (id "${id}" já existe).`
+  } else if (typeof name !== "string" || regex.test(name)){
+    return "Por favor. Digite um nome válido!"
+  } else if (client === undefined) {
+    clients.push({id: Number(id), name: name})
+    return clients
+  }
+}
+console.log("Exercicio 1: Cadastro de cliente")
+console.log(newClient("5","batata"))
+
+
+
+
+// exercicio 2 Geração de tabuada //
+const creatTabuada = (quantity) => {
+  if(typeof quantity !== "number"){
+    return console.log(`Erro. Parâmetro inválido ('Digite um número')`)
+  }else if (quantity > 10 || quantity < 1) {
+    return console.log(`Erro. Parâmetro inválido ('número precisa valer entre 1 e 10')`)
+  } else {
+    for(let i = 0; i <= quantity; i++){
+      console.log(quantity * i)
+    }
+  }
+}
+console.log("Exercicio 2: Gerar uma tabuada")
+creatTabuada(8)
--- modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -1,1 +1,28 @@
 console.log("exercicio 4");
+
+const firtList = [
+  { name: "Banana" },
+  { name: "Laranja" }
+]
+const secondList = [
+  {name: "Laranja"},
+  {name: "Cebola"}
+]
+
+const newList = []
+const generateUniqueItems = (item1, item2) => {
+  const seeItem1 = item1.map( name => name.name)
+  const seeItem2 = item2.map( name => name.name)
+  // const seeFirtList = firtList.find( item => item1.name === seeItem1)
+  // // console.log(seeFirtList)
+  // let test;
+  for( i = 0; i < item1.length; i++){
+    newList.push(item1[i].name) 
+  }
+  // item2.filter( iten => iten.name !==)
+  // return console.log(seeItem1,seeItem2)
+} 
+generateUniqueItems(firtList,secondList)
+console.log(newList)
+// firtList.map( test => console.log(test.name.includes("Banana")))
+
